05a153add169af54f5832e6c60685bf05ef233d1
hazzik <hazzik@gmail.com>
2009-04-03 11:18:50 +0000

    * implemented ICollection<ITriangleList> on TriangleListCollection
    git-svn-id: http://localhost:88/svn/uwow/mpqnav/trunk@391 3b753528-4e94-6941-993a-e4fe79dda6b7

--- MPQNav/Graphics/TriangleListCollection.cs
+++ MPQNav/Graphics/TriangleListCollection.cs
@@ -1,13 +1,60 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace MPQNav.Graphics {
-	internal class TriangleListCollection : ITriangleList {
+	internal class TriangleListCollection : ICollection<ITriangleList>, ITriangleList {
 		private readonly IList<int> _indices = new List<int>();
 		private readonly IList<ITriangleList> _lists = new List<ITriangleList>();
 		private readonly IList<VertexPositionNormalColored> _vertices = new List<VertexPositionNormalColored>();
 		private bool _durty;
 		private int _offset;
+
+		#region ICollection<ITriangleList> Members
+
+		public void Add(ITriangleList triangeList) {
+			_lists.Add(triangeList);
+			_durty = true;
+		}
+
+		public void Clear() {
+			_lists.Clear();
+			_durty = true;
+		}
+
+		public bool Contains(ITriangleList item) {
+			return _lists.Contains(item);
+		}
+
+		public void CopyTo(ITriangleList[] array, int arrayIndex) {
+			_lists.CopyTo(array, arrayIndex);
+		}
+
+		public bool Remove(ITriangleList item) {
+			if(_lists.Remove(item)) {
+				_durty = true;
+				return true;
+			}
+			return false;
+		}
+
+		public int Count {
+			get { return _lists.Count; }
+		}
+
+		public bool IsReadOnly {
+			get { return _lists.IsReadOnly; }
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return GetEnumerator();
+		}
+
+		IEnumerator<ITriangleList> IEnumerable<ITriangleList>.GetEnumerator() {
+			return GetEnumerator();
+		}
+
+		#endregion
 
 		#region ITriangleList Members
 
@@ -35,6 +82,10 @@ namespace MPQNav.Graphics {
 
 		#endregion
 
+		public IEnumerator<ITriangleList> GetEnumerator() {
+			return _lists.GetEnumerator();
+		}
+
 		private void JoinAll() {
 			_vertices.Clear();
 			_indices.Clear();
@@ -48,11 +99,6 @@ namespace MPQNav.Graphics {
 				_offset = _vertices.Count;
 			}
 			_durty = false;
-		}
-
-		public void Add(ITriangleList triangeList) {
-			_lists.Add(triangeList);
-			_durty = true;
 		}
 	}
 }
